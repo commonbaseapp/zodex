8db8d01b5b0d97a175009bbf3764b4f368723898
Jade Devin Cabatlao <jadedevin13@users.noreply.github.com>
2024-06-18 00:44:50 +0200

    Update zod-types.ts

--- src/zod-types.ts
+++ src/zod-types.ts
@@ -1,9 +1,9 @@
-import { z } from "zod";
+import { DIRTY, INVALID, OK, z, ZodType, ZodTypeDef } from 'zod'
 
 type Modifiers =
   | z.ZodOptional<ZodTypes>
   | z.ZodNullable<ZodTypes>
-  | z.ZodDefault<ZodTypes>;
+  | z.ZodDefault<ZodTypes>
 
 type Primitives =
   | z.ZodString
@@ -17,24 +17,24 @@ type Primitives =
   | z.ZodAny
   | z.ZodUnknown
   | z.ZodNever
-  | z.ZodVoid;
+  | z.ZodVoid
 
 type ListCollections =
   | z.ZodTuple<any, any>
   | z.ZodSet<ZodTypes>
-  | z.ZodArray<ZodTypes>;
+  | z.ZodArray<ZodTypes>
 
 type KVCollections =
   | z.ZodObject<any>
   | z.ZodRecord<any, ZodTypes>
-  | z.ZodMap<ZodTypes, ZodTypes>;
+  | z.ZodMap<ZodTypes, ZodTypes>
 
 type ADTs =
   | z.ZodUnion<readonly [ZodTypes, ...ZodTypes[]]>
   | z.ZodDiscriminatedUnion<any, z.ZodObject<{ [k: string]: ZodTypes }>[]>
   | z.ZodIntersection<ZodTypes, ZodTypes>
   | z.ZodNativeEnum<any>
-  | z.ZodEnum<any>;
+  | z.ZodEnum<any>
 
 export type ZodTypes =
   | Modifiers
@@ -42,6 +42,7 @@ export type ZodTypes =
   | ListCollections
   | KVCollections
   | ADTs
+  | ZodRef
   | z.ZodFunction<any, ZodTypes>
   | z.ZodLazy<ZodTypes>
   | z.ZodLiteral<any>
@@ -49,6 +50,35 @@ export type ZodTypes =
   | z.ZodCatch<ZodTypes>
   | z.ZodPromise<ZodTypes>
   | z.ZodBranded<ZodTypes, any>
-  | z.ZodPipeline<ZodTypes, ZodTypes>;
+  | z.ZodPipeline<ZodTypes, ZodTypes>
 
-export type ZTypeName<T extends ZodTypes> = T["_def"]["typeName"];
+export type ZTypeName<T extends ZodTypes> = T['_def']['typeName']
+
+
+interface ZodRefDef extends ZodTypeDef {
+  typeName: 'ZodRef';
+  schema: ZodType;
+}
+
+export class ZodRef extends ZodType<unknown, ZodRefDef> {
+  _parse(input: z.ParseInput): z.ParseReturnType<unknown> {
+    const { ctx, status } = this._processInputParams(input);
+    if (status.value === "valid") {
+      return OK(ctx.data);
+    } else if (status.value === "dirty") {
+      return DIRTY(ctx.data);
+    }
+    return INVALID;
+  }
+
+  get schema() {
+    return this._def.schema;
+  }
+
+  static create(schema: ZodType): ZodRef {
+    return new ZodRef({
+      typeName: 'ZodRef',
+      schema,
+    });
+  }
+}
