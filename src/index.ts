7048838b48f747cc0359a78c0d32a6a986a22e92
Gregor <mail@dflate.io>
2024-09-05 22:48:44 +0200

    fix: remove ui code (#42)

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ export * from "./dezerialize";
 export * from "./zerialize";
 
 export * from "./types";
-export { mapTypesToViews } from "./ui";
 
 type KeysOfUnion<T> = T extends T ? keyof T : never;
 export type SzPropertyKeysOf<T extends SzType> = KeysOfUnion<
