7eb3842c9d3d2688e179321bd9cbff43292f93f8
miunau <miunau@miunau.com>
2025-08-18 12:54:47 +0200

    fix: export types, add .js to imports for nodenext compatibility

--- lib/index.ts
+++ lib/index.ts
@@ -1,11 +1,11 @@
-import { SzType } from "./types";
+import { SzType } from "./types.js";
 
-export * from "./dezerialize";
-export * from "./zerialize";
+export * from "./dezerialize.js";
+export * from "./zerialize.js";
 
-export * from "./types";
+export * from "./types.js";
 
-type KeysOfUnion<T> = T extends T ? keyof T : never;
+export type KeysOfUnion<T> = T extends T ? keyof T : never;
 export type SzPropertyKeysOf<T extends SzType> = KeysOfUnion<
   Extract<T, { type: "object" }>["properties"]
 >;
