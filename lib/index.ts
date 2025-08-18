c0773239fc9514fd516083ee45cf7fbbc43fb8de
miunau <miunau@miunau.com>
2025-08-18 19:51:27 +0200

    fix: also export zod-types.js

--- lib/index.ts
+++ lib/index.ts
@@ -4,6 +4,7 @@ export * from "./dezerialize.js";
 export * from "./zerialize.js";
 
 export * from "./types.js";
+export * from "./zod-types.js";
 
 export type KeysOfUnion<T> = T extends T ? keyof T : never;
 export type SzPropertyKeysOf<T extends SzType> = KeysOfUnion<
